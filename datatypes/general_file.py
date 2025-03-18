8b420e7762d69fcbd34986eaa0ef1ed502052e35
shikhar-213 <213shikharsharma@gmail.com>
2025-03-18 11:06:21 +0530

    adding coordinates program

--- datatypes/general_file.py
+++ datatypes/general_file.py
@@ -1,12 +1,16 @@
 from Fractions.fractions import Fraction
-x = Fraction(7,2)
-y = Fraction(3,8)
-print(x)
-print(y)
-print(f"Type of {x}: {type(x)}")
-print(f"Type of {y}: {type(y)}")
-# Operations
-print(x + y)
-print(x - y)
-print(x * y)
-print(x / y)
+from Coordinates.coordinates import Coordinate2D
+# x = Fraction(7,2)
+# y = Fraction(3,8)
+# print(x)
+# print(y)
+# print(f"Type of {x}: {type(x)}")
+# print(f"Type of {y}: {type(y)}")
+# # Operations
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+c = Coordinate2D(2,5)
+print(f"Type of point A{c}: {type(c)}")
