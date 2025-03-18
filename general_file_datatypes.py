01f2ff782a2c8059ccb54f970a4733ef483cf89e
shikhar-213 <213shikharsharma@gmail.com>
2025-03-18 17:08:26 +0530

    files_locations_changed

--- general_file_datatypes.py
+++ general_file_datatypes.py
@@ -1,16 +1,17 @@
 from Fractions.fractions import Fraction
 from Coordinates.coordinates import Coordinate2D
+# # Fractions
 # x = Fraction(7,2)
 # y = Fraction(3,8)
 # print(x)
 # print(y)
-# print(f"Type of {x}: {type(x)}")
-# print(f"Type of {y}: {type(y)}")
-# # Operations
 # print(x + y)
 # print(x - y)
 # print(x * y)
 # print(x / y)
+# print(f"Type of {x}: {type(x)}")
+# print(f"Type of {y}: {type(y)}")
 
-c = Coordinate2D(2,5)
-print(f"Type of point A{c}: {type(c)}")
+c = Coordinate2D(2, 3)
+print(c)
+print(type(c))
